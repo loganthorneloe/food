e5b42f70ef93126574152b21d088ca332a7940e5
Logan Thorneloe <loganthorneloe@sylvester.lan>
2024-01-15 14:44:52 -0600

    better logic

--- plan.py
+++ plan.py
@@ -1,7 +1,14 @@
 import random
 import yaml
 from datetime import datetime, timedelta
+import sys
 
+'''
+generated requirements.txt with:
+pip install pipreqs
+pipreqs /path/to/project
+'''
+path_to_yaml = "food.yaml"
 weekday_names = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 def plan_meals(protein, vegs, carbs):
@@ -25,7 +32,7 @@ def plan_meals(protein, vegs, carbs):
         meat = random.choice(protein)
 
     block_list.append(meat)
-    shopping_list.append(meat)
+    shopping_list.append(meat.replace("_"," "))
 
     # grab veg
     veg = random.choice(vegs)
@@ -33,7 +40,7 @@ def plan_meals(protein, vegs, carbs):
       veg = random.choice(vegs)
 
     block_list.append(veg)
-    shopping_list.append(veg)
+    shopping_list.append(veg.replace("_"," "))
 
     # grab carb
     carb = random.choice(carbs)
@@ -41,7 +48,7 @@ def plan_meals(protein, vegs, carbs):
       carb = random.choice(carbs)
 
     block_list.append(carb)
-    shopping_list.append(carb)
+    shopping_list.append(carb.replace("_"," "))
 
     meal = {'carb': carb, 'meat': meat, 'veg': veg}
     meal_plan.append(meal)
@@ -49,7 +56,7 @@ def plan_meals(protein, vegs, carbs):
   return meal_plan, shopping_list
 
 def organize_meals(meals, days):
-  new_meals = {"Tuesday":"Chicken nuggets with mac and cheese", "Friday": "Costco pizza", "Saturday": "audible or leftovers"}
+  new_meals = {"Tuesday":"Chicken nuggets with mac and cheese", "Friday": "costco pizza", "Saturday": "audible or leftovers"}
   format_string = "{} with {} and {}"
   for meal in meals:
     meal_string = format_string.format(meal['meat'], meal['carb'], meal['veg'])
@@ -79,13 +86,13 @@ def assign_to_date(meals, prep_list):
       prep_day = prep_date.day
       prep_year = prep_date.year
       prep_full = format_date_string.format(prep_month, prep_day, prep_year) + " 6:00"
-      reminders[prep_full] = "Marinate Chicken?"
+      reminders[prep_full] = "Food prep for tomorrow's dinner"
     reminders[meal_date] = meals[weekday].replace("_"," ")
 
   return reminders
 
 def pull_from_yaml():
-  data = yaml.safe_load(open('food.yaml','r'))
+  data = yaml.safe_load(open(path_to_yaml,'r'))
   return data['protein'], data['vegs'], data['carbs'], data['days'], data['prep_list']
 
 def plan_pasta(vegs):
@@ -96,13 +103,38 @@ def plan_pasta(vegs):
   pasta_meal = {'carb': 'pasta', 'meat': meat, 'veg': veg}
   return pasta_meal, meat, veg
 
+def meals_stringbuilder(reminders):
+  total_reminders_string = ""
+  for key, value in reminders.items():
+    new_string = key + "*" + value
+    total_reminders_string += new_string + "|"
+  return total_reminders_string[:-1]
+
+def shopping_list_stringbuilder(shopping_list):
+  total_shopping_string = ""
+  for item in shopping_list:
+    total_shopping_string += item + "|"
+  return total_shopping_string[:-1]
+
+
+arguments = sys.argv
+try:
+  if sys.argv[1] == "remote":
+    path_to_yaml = 'src/food/food.yaml'
+except:
+  pass
+
 protein, vegs, carbs, days, prep_list = pull_from_yaml()
 meals, shopping_list = plan_meals(protein, vegs, carbs)
 new_meals = organize_meals(meals, days)
 reminders = assign_to_date(new_meals, prep_list)
 
-
-ret = {}
-ret['reminders'] = reminders
-ret['shopping'] = shopping_list
+ret = meals_stringbuilder(reminders) + "-" + shopping_list_stringbuilder(shopping_list)
 print(ret)
+
+# split_reminder_and_shopping = ret.split("-")
+# rmd = split_reminder_and_shopping[0].split("|")
+# spl = split_reminder_and_shopping[1].split(" ")
+
+# print(rmd)
+# print(spl)
